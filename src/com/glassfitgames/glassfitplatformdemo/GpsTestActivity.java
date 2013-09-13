5d366c5fe8fb9f8efe9cc681878b340eaf3066ae
Ben <ben.lister@glassfitgames.com>
2013-09-13 20:18:04 +0100

    BL: Updated GpsTestActivity to reflect new GPSHelper

--- src/com/glassfitgames/glassfitplatformdemo/GpsTestActivity.java
+++ src/com/glassfitgames/glassfitplatformdemo/GpsTestActivity.java
@@ -1,96 +1,92 @@
+
 package com.glassfitgames.glassfitplatformdemo;
 
-import java.io.File;
-
 import android.app.Activity;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.glassfitgames.glassfitplatform.gpstracker.GPSTracker;
+import com.glassfitgames.glassfitplatform.gpstracker.TargetTracker;
 import com.glassfitgames.glassfitplatform.gpstracker.Helper;
-import com.glassfitgames.glassfitplatform.models.Position;
-import com.roscopeco.ormdroid.ORMDroidApplication;
 
 public class GpsTestActivity extends Activity {
 
-	private Helper gpsHelper;
-	private TextView testLocationText;
-	private Button initGpsButton;
-	private Button startTrackingButton;
-	private Button stopTrackingButton;
-	private Button getCurrentPositionButton;
-	private Button distanceButton;
-	private boolean targetset = false;
+    private GPSTracker gpsTracker;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.testgps);
-		testLocationText = (TextView) findViewById(R.id.testLocationText);
-		initGpsButton = (Button) findViewById(R.id.initGpsButton);
-		startTrackingButton = (Button) findViewById(R.id.startTrackingButton);
-		stopTrackingButton = (Button) findViewById(R.id.stopTrackingButton);
-		getCurrentPositionButton = (Button) findViewById(R.id.getCurrentPositionButton);
-		distanceButton = (Button) findViewById(R.id.DistanceButton);
+    private TargetTracker targetTracker;
 
-		gpsHelper = new Helper(getApplicationContext());
+    private TextView testLocationText;
 
-		initGpsButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				gpsHelper.initGps();
-			}
-		});
+    private Button initGpsButton;
 
-		startTrackingButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				gpsHelper.startTracking();
-			}
-		});
+    private Button initTargetButton;
 
-		stopTrackingButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				gpsHelper.stopTracking();
-			}
-		});
+    private Button startTrackingButton;
 
-		getCurrentPositionButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Position pos;
-				try {
-					pos = gpsHelper.getCurrentPosition();
-					String lat = Double.toString(pos.latx);
-					String lon = Double.toString(pos.lngx);
-					String text = "Current Latitude is : " + lat
-							+ " and Current Longtitude is: " + lon;
-					testLocationText.setText(text);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+    private Button stopTrackingButton;
 
-			}
-		});
+    private Button distanceButton;
 
-		distanceButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (!targetset) {
-					gpsHelper.setTargetTrack(0);
-					targetset = true;
-				}
-				long distance = gpsHelper.getTargetElapsedDistance();
-				String text = "Elapsed distance: " + distance;
-				testLocationText.setText(text);
-			}
-		});
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testgps);
+        testLocationText = (TextView)findViewById(R.id.testLocationText);
+        initGpsButton = (Button)findViewById(R.id.initGpsButton);
+        initTargetButton = (Button)findViewById(R.id.initTargetButton);
+        startTrackingButton = (Button)findViewById(R.id.startTrackingButton);
+        stopTrackingButton = (Button)findViewById(R.id.stopTrackingButton);
+        distanceButton = (Button)findViewById(R.id.DistanceButton);
 
-	}
+        initGpsButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gpsTracker = Helper.getGPSTracker(getApplicationContext());
+            }
+        });
+
+        initTargetButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    targetTracker = Helper.getTargetTracker("pb");
+                } catch (Exception e) {
+                    Log.e("GlassFitPlatform", e.getMessage());
+                }
+            }
+        });
+
+        startTrackingButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gpsTracker.startTracking();
+            }
+        });
+
+        stopTrackingButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gpsTracker.stopTracking();
+            }
+        });
+
+        distanceButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long gpsDistance = gpsTracker.getElapsedDistance();
+                long gpsTime = gpsTracker.getElapsedTime();
+                long targetDistance = targetTracker.getCumulativeDistanceAtTime(gpsTime);
+
+                String text = "Elapsed distance: " + gpsDistance + ". Target distance = "
+                        + targetDistance;
+                testLocationText.setText(text);
+            }
+        });
+
+    }
 
 }
